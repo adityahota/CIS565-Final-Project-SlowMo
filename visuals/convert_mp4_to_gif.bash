cfeab99f4169527e7d6f79fa38145150af3587fb
kaan <kaan803@gmail.com>
2021-12-13 05:40:44 -0500

    fixed gif images

--- visuals/convert_mp4_to_gif.bash
+++ visuals/convert_mp4_to_gif.bash
@@ -4,8 +4,7 @@
 # Copyright Bhupesh Varshney
 # Utility to convert video files to GIFs using ffmpeg
 #
-# Usage: convert-to-gif.sh <video-file-path>
-# To skip frames: convert-to-gif.sh <video-file-path> <time-in-seconds>
+# Usage: convert-to-gif.sh <video-file-path> <fps>
 # Example:
 # convert-to-gif.sh video.mp4 28
 
@@ -13,6 +12,11 @@
 if [[ -z "$1" ]]; then
     echo "No video file specified"
     exit 1
+fi
+
+if [[ -z "$2" ]]; then
+    echo "No fps specified"
+    exit 2
 fi
 
 # get everything after last /
@@ -26,15 +30,11 @@ dimensions=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,hei
 
 echo -e "$(tput bold)Generating Palette $(tput sgr0)"
 # Generate palette
-ffmpeg -i "$1" -vf "fps=22,scale=${dimensions%x*}:-1:flags=lanczos,palettegen" "$filename".png
+ffmpeg -i "$1" -vf "fps=$2,scale=${dimensions%x*}:-1:flags=lanczos,palettegen" "$filename".png
 
 echo -e "$(tput bold)Converting Video to GIF $(tput sgr0)"
 
-if [[ "$2" ]]; then
-    ffmpeg -t "$2" -i "$1" -i "$filename".png -filter_complex "fps=22,scale=${dimensions%x*}:-1:flags=lanczos[x];[x][1:v]paletteuse" "$filename".gif
-else
-    ffmpeg -i "$1" -i "$filename".png -filter_complex "fps=22,scale=${dimensions%x*}:-1:flags=lanczos[x];[x][1:v]paletteuse" "$filename".gif
-fi
+ffmpeg -i "$1" -i "$filename".png -filter_complex "fps=$2,scale=${dimensions%x*}:-1:flags=lanczos[x];[x][1:v]paletteuse" "$filename".gif
 
 echo -e "Removing palette"
 rm "$filename".png
