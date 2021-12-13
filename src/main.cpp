d815b5453ebe8eed71b8ecedc5e4f105ca44c750
Richard Chen <richard.rl.chen@gmail.com>
2021-12-12 19:07:36 -0500

    Begin testing encoder

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 
 #include "includes.h"
 #include "flavr.h"
+#include "bstem.h"
 
 int main()
 {
+    string stacked_frames_file = "/home/aditya/Documents/Development/cis565/Final-Project/CUDA-Convolution2D/example-frames/frames_post__1x3x4x256x448.bin";
+
+    // Read stacked input size
+    Dims5 input_dims5 = filename2dims5(stacked_frames_file);
+
     return 0;
 }
